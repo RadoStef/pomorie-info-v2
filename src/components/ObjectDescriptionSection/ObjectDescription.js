90d87ed04b2385609cdeb9d31a8126aa81b071ba
RadoStef <rs_perfect@mail.bg>
2023-09-06 00:10:33 +0300

    minor design changes

--- src/components/ObjectDescriptionSection/ObjectDescription.js
+++ src/components/ObjectDescriptionSection/ObjectDescription.js
@@ -10,27 +10,27 @@ export const ObjectDescription = ({ images, text, objectName }) => {
   };
 
   return (
-    <>
-      <div className={styles['section-wrapper']}>
-        <div className={styles['description-wrapper']}>
-          <h3>{objectName}</h3>
-          <div>{text}</div>
-        </div>
-        <div>
-          <FsLightbox toggler={toggler} sources={images} />
-        </div>
-        <div className={styles['pictures-wrapper']}>
-          <img
-            className={styles['lightbox-img']}
-            src={images[0]}
-            alt={objectName}
-            onClick={toggleHandler}
-          />
+    <div className={styles['section-wrapper']}>
+      <div className={styles['description-wrapper']}>
+        <h3>{objectName}</h3>
+        <div>{text}</div>
+      </div>
+      <div>
+        <FsLightbox toggler={toggler} sources={images} />
+      </div>
+      <div className={styles['pictures-wrapper']}>
+        <img
+          className={styles['lightbox-img']}
+          src={images[0]}
+          alt={objectName}
+          onClick={toggleHandler}
+        />
+        <div className={styles['button-layer']}>
           <button className={styles['gallery-btn']} onClick={toggleHandler}>
             фото галерия
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
